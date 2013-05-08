Rotation implemented + correct superposition

src/draw.py
@@ -1,8 +1,9 @@
-#!/usr/bin/python
+#!/opt/local/bin/python
 #coding=utf-8
 from PySide.QtCore import *
 from PySide.QtGui import *
 from PySide.QtOpenGL import *
+import numpy as np
 
 import sys, os
 import io
@@ -11,9 +12,6 @@ import io
 import pyLF_DEM_posfile_reading
 
 
-def rotateConfiguration(pos_array, angleX, angleY):
-
-    rotation
 
 class drawConfiguration(QGLWidget):
     speed=10
@@ -22,8 +20,6 @@ class drawConfiguration(QGLWidget):
         QGLWidget.__init__(self, parent)
         # setGeometry(x_pos, y_pos, width, height)
         
-        self.setGeometry(400, 400, 850, 850)
-        self.setWindowTitle('omer viewer')
 
         self.timer = QBasicTimer()
     
@@ -33,16 +29,25 @@ class drawConfiguration(QGLWidget):
 
         self.positions=dict()
 
-        self.centerX = +0.5*self.L[0]
-        self.centerY = -0.5*self.L[2]
+        ratio = self.L[2]/self.L[0]
+        sizeX = 800
+        sizeY = sizeX*ratio
+
+        self.setGeometry(400, 400, sizeX, sizeY)
+
         self.scaleX = self.width()/self.L[0]
         self.scaleY = self.height()/self.L[2]
+
+        self.setWindowTitle('omer viewer')
+
+        print self.width(), self.height()
 #        self.connect(self.timer, SIGNAL("timeout()"), self.update)
 
-        self.transform = QTransform()
-        self.rotation = QGraphicsRotation()
-        
+        self.rotation = np.mat([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
+
         spheres = [ QGraphicsEllipseItem(0,0,50,50) ]
+
+
     def start(self):
         self.timer.start(drawConfiguration.speed,self)
 
@@ -66,7 +71,7 @@ class drawConfiguration(QGLWidget):
 
     def keyPressEvent(self, event):
         if event.key() == Qt.Key_R:
-            self.transform.rotate(45, Qt.YAxis)
+            self.rotation = np.identity(3)
             self.update()
 
     def mousePressEvent(self, event):
@@ -77,9 +82,20 @@ class drawConfiguration(QGLWidget):
         self.previous_point = self.current_point
         self.current_point = event.posF()
         
-        self.angleY = self.current_point.x() - self.previous_point.x()
-        self.transform.rotate(self.angleY, Qt.YAxis)
+        angleY = -(self.current_point.x() - self.previous_point.x())/self.width()
         
+        sinAngleY = np.sin(angleY)
+        cosAngleY = np.cos(angleY)
+        generator = np.mat([[cosAngleY, -sinAngleY, 0], [sinAngleY, cosAngleY, 0], [0, 0, 1]])
+        self.rotation = generator*self.rotation
+
+        angleX = -(self.current_point.y() - self.previous_point.y())/self.height()
+        
+        sinAngleX = np.sin(angleX)
+        cosAngleX = np.cos(angleX)
+        generator = np.mat([[1, 0, 0], [0, cosAngleX, -sinAngleX], [0, sinAngleX, cosAngleX]])
+        self.rotation = generator*self.rotation
+
         self.update()
             
             
@@ -90,29 +106,41 @@ class drawConfiguration(QGLWidget):
         paint = QPainter()
         paint.begin(self)
 
-        # optional
-#        paint.setRenderHint(QPainter.Antialiasing)
-        paint.setTransform(self.transform)
+
+        paint.setRenderHint(QPainter.Antialiasing)
         # make a white drawing background
         paint.setBrush(Qt.white)
         paint.drawRect(event.rect())
 
-        # draw red circles
+        # draw yellow circles
         paint.setPen(Qt.black)
         paint.setBrush(Qt.yellow)
         
-#        for i in self.pos_stream.range():
-        positions=self.pos_stream.positions
+        if len(self.pos_stream.positions)==0:
+            return
+        
+        positions = np.concatenate([self.pos_stream.positions[str(i)] for i in self.pos_stream.range() ])
 
-        for i in positions:
+        pos = (positions*self.rotation)
+
+        objects_to_paint = []
+        for i in range(len(pos)):
             
-            radx = self.pos_stream.radius[i]*self.width()/self.L[0]
+            radx = self.pos_stream.radius[str(i)]*self.scaleX
             rady = radx
-            pointX=(positions[i][0]+self.centerX)*self.scaleX
-            pointY=-(positions[i][2]+self.centerY)*self.scaleY
 
-            center = QPoint(pointX, pointY)
-            paint.drawEllipse(center, radx, rady)
+            pointX=pos[i].item(0)*self.scaleX
+            pointY=-pos[i].item(2)*self.scaleY
+
+            center = QRectF(pointX, pointY, 2*radx, 2*rady)
+
+            objects_to_paint.append([-pos[i].item(1), center])
+            
+        objects_to_paint.sort(key=lambda obj: obj[0]) # draw objects in front last
+
+        paint.setTransform(QTransform().translate(0.5*self.width(), 0.5*self.height()))
+        for obj in objects_to_paint:
+            paint.drawEllipse(obj[1])
 
         paint.end()
 