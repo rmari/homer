d3c76ef18173b9cdf12f303556fc870b32dfe717
Romain Mari <romain.mari@gmail.com>
2014-09-02 18:05:19 -0400

    halfway through optimization

--- src/homerFile.py
+++ src/homerFile.py
@@ -33,49 +33,84 @@ class homerFile:
         return self.max[2]-self.min[2]
 
 
+    def parse_raw_frame(self, raw_frame):
+        self.objects = obj
+        obj_nb = (self.objects[:,0].shape)[0]
 
-    def atparse(self,values):
-        col=int(values[1])
-        if col > 5:
-            col=0
+        self.pos1_ind = 1
+        self.pos2_ind = 4
 
-        self.color = self.colordef[str(col)]
+        self.size_ind = 7
+        self.color_ind = 8
+        self.layer_ind = 9
+
+        self.bare_positions = np.array(self.objects[:,self.pos1_ind:], dtype=np.float64)
+        self.bare_positions[:,[1,2,4,5]] = -self.bare_positions[:,[1,2,4,5]]
+
+        size_pos = np.nonzero(self.objects[:,0] == command_coding['r'])[0]
+        self.bare_sizes = np.zeros(obj_nb)
+
+        for i in range(len(size_pos)-1):
+            self.bare_sizes[size_pos[i]:size_pos[i+1]] = self.objects[size_pos[i],1]
+        self.bare_sizes[size_pos[-1]:] = self.objects[size_pos[-1],1]
+
+        color_pos = np.nonzero(self.objects[:,0] == command_coding['@'])[0]
+        self.bare_colors = np.empty(obj_nb, dtype=np.int)
+
+        for i in range(len(color_pos)-1):
+            self.bare_colors[color_pos[i]:color_pos[i+1]] = int(self.objects[color_pos[i],1])
+        self.bare_colors[color_pos[-1]:] = int(self.objects[color_pos[-1],1])
+
+        layer_pos = np.nonzero(self.objects[:,0] == command_coding['y'])[0]
+        self.bare_layers = np.empty(obj_nb, dtype=np.int)
+
+        for i in range(len(color_pos)-1):
+            self.bare_layers[layer_pos[i]:layer_pos[i+1]] = int(self.objects[layer_pos[i],1])
+        self.bare_layers[layer_pos[-1]:] = int(self.objects[layer_pos[-1],1])
+        
+        self.objects = np.hstack((self.objects, np.reshape(self.bare_sizes,(obj_nb,1)), np.reshape(self.bare_colors,(obj_nb,1)), np.reshape(self.bare_layers,(obj_nb,1))))
+
+        # remove non-object commands
+        real_obj_indices = -np.isnan(self.bare_positions[:,2])
+        self.objects = self.objects[real_obj_indices]
+        self.bare_positions = self.bare_positions[real_obj_indices]
+        self.bare_sizes = self.bare_sizes[real_obj_indices]
 
 
     def read_chunk(self):
         if self.read_all:
             return False
 
-        myframe = self.reader.get_chunk(self.chunksize)
+        in_raw_data = self.reader.get_chunk(self.chunksize)
 
-        if myframe.shape[0] < self.chunksize:
+        if in_raw_data.shape[0] < self.chunksize:
             self.read_all = True
-        if myframe.empty:
+        if in_raw_data.empty:
             return False
 
         for k in command_coding:
-            myframe.replace(to_replace=k,value=command_coding[k],inplace=True)
+            in_raw_data.replace(to_replace=k,value=command_coding[k],inplace=True)
 
-        myframe.astype(np.float)
-        splitpoints = np.nonzero(np.array(pd.isnull(myframe['a'])))[0]
+        in_raw_data.astype(np.float)
+        framepoints = np.nonzero(np.array(pd.isnull(in_raw_data['a'])))[0]
 
-        whole_array = np.array(myframe)
-        split_array = np.split(whole_array, splitpoints)
+        whole_array = np.array(in_raw_data)
+        raw_data_frames = np.split(whole_array, framepoints)
         
-        frame = split_array[0]
+        frame = raw_data_frames[0]
         if self.init:
             self.init = False
         else:
             self.frames.append(homerFrame.homerFrame(np.vstack((self.truncated_array, frame))))
             
-        for frame in split_array[1:-1]:
+        for frame in raw_data_frames[1:-1]:
             self.frames.append(homerFrame.homerFrame(frame))
 
-        self.truncated_array = split_array[-1]
+        self.truncated_array = raw_data_frames[-1]
 
 
-        self.max = np.array([myframe['p1'].max(), myframe['p2'].max(), myframe['p3'].max()])
-        self.min = np.array([myframe['p1'].min(), myframe['p2'].min(), myframe['p3'].min()])
+        self.max = np.array([in_raw_data['p1'].max(), in_raw_data['p2'].max(), in_raw_data['p3'].max()])
+        self.min = np.array([in_raw_data['p1'].min(), in_raw_data['p2'].min(), in_raw_data['p3'].min()])
 
         return True
 
--- src/homerFrame.py
+++ src/homerFrame.py
@@ -6,12 +6,6 @@ from PySide.QtOpenGL import *
 #from numba import autojit
 #from numba import jit, void, int_, float32
 
-pos1_ind = 1
-pos2_ind = 4
-
-size_ind = 7
-color_ind = 8
-layer_ind = 9
 
 command_coding = { 'y':0, '@':1, 'r':3, 'c':4, 'l':5, 's':6 }
 fidelity_scale = [ Qt.SolidPattern, Qt.Dense2Pattern, Qt.Dense6Pattern, Qt.NoBrush ]
@@ -26,8 +20,8 @@ class homerFrame:
         self.populate(obj)
 
     def populate(self, obj):
-        self.objects = obj
-        obj_nb = (self.objects[:,0].shape)[0]
+
+        obj_nb = (obj[:,0].shape)[0]
 
         self.pos1_ind = 1
         self.pos2_ind = 4
@@ -36,51 +30,63 @@ class homerFrame:
         self.color_ind = 8
         self.layer_ind = 9
 
-        self.bare_positions = np.array(self.objects[:,self.pos1_ind:], dtype=np.float64)
+        self.bare_positions = np.array(obj[:,self.pos1_ind:], dtype=np.float64)
         self.bare_positions[:,[1,2,4,5]] = -self.bare_positions[:,[1,2,4,5]]
 
-        size_pos = np.nonzero(self.objects[:,0] == command_coding['r'])[0]
+        size_pos = np.nonzero(obj[:,0] == command_coding['r'])[0]
         self.bare_sizes = np.zeros(obj_nb)
 
         for i in range(len(size_pos)-1):
-            self.bare_sizes[size_pos[i]:size_pos[i+1]] = self.objects[size_pos[i],1]
-        self.bare_sizes[size_pos[-1]:] = self.objects[size_pos[-1],1]
+            self.bare_sizes[size_pos[i]:size_pos[i+1]] = obj[size_pos[i],1]
+        self.bare_sizes[size_pos[-1]:] = obj[size_pos[-1],1]
 
-        color_pos = np.nonzero(self.objects[:,0] == command_coding['@'])[0]
+        color_pos = np.nonzero(obj[:,0] == command_coding['@'])[0]
         self.bare_colors = np.empty(obj_nb, dtype=np.int)
 
         for i in range(len(color_pos)-1):
-            self.bare_colors[color_pos[i]:color_pos[i+1]] = int(self.objects[color_pos[i],1])
-        self.bare_colors[color_pos[-1]:] = int(self.objects[color_pos[-1],1])
+            self.bare_colors[color_pos[i]:color_pos[i+1]] = int(obj[color_pos[i],1])
+        self.bare_colors[color_pos[-1]:] = int(obj[color_pos[-1],1])
 
-        layer_pos = np.nonzero(self.objects[:,0] == command_coding['y'])[0]
+        layer_pos = np.nonzero(obj[:,0] == command_coding['y'])[0]
         self.bare_layers = np.empty(obj_nb, dtype=np.int)
 
         for i in range(len(color_pos)-1):
-            self.bare_layers[layer_pos[i]:layer_pos[i+1]] = int(self.objects[layer_pos[i],1])
-        self.bare_layers[layer_pos[-1]:] = int(self.objects[layer_pos[-1],1])
+            self.bare_layers[layer_pos[i]:layer_pos[i+1]] = int(obj[layer_pos[i],1])
+        self.bare_layers[layer_pos[-1]:] = int(obj[layer_pos[-1],1])
         
-        self.objects = np.hstack((self.objects, np.reshape(self.bare_sizes,(obj_nb,1)), np.reshape(self.bare_colors,(obj_nb,1)), np.reshape(self.bare_layers,(obj_nb,1))))
+        self.objects = np.hstack((obj, np.reshape(self.bare_sizes,(obj_nb,1)), np.reshape(self.bare_colors,(obj_nb,1)), np.reshape(self.bare_layers,(obj_nb,1))))
+        all_objects = np.hstack((obj, np.reshape(self.bare_sizes,(obj_nb,1)), np.reshape(self.bare_colors,(obj_nb,1)), np.reshape(self.bare_layers,(obj_nb,1))))
 
         # remove non-object commands
         real_obj_indices = -np.isnan(self.bare_positions[:,2])
         self.objects = self.objects[real_obj_indices]
+        all_objects = all_objects[real_obj_indices]
         self.bare_positions = self.bare_positions[real_obj_indices]
         self.bare_sizes = self.bare_sizes[real_obj_indices]
 
-        self.obj_nb = (self.objects[:,0].shape)[0]
+        self.obj_nb = (all_objects[:,0].shape)[0]
 
         self.painter_methods = np.empty((self.obj_nb,4), dtype=np.object)
 
 
-        self.circles_labels = np.nonzero(self.objects[:,0] == command_coding['c'])[0]
+        self.circles_labels = np.nonzero(all_objects[:,0] == command_coding['c'])[0]
         self.painter_methods[self.circles_labels,0] = 1
-        self.lines_labels = np.nonzero(self.objects[:,0] == command_coding['l'])[0]
+        self.circles = all_objects[self.circles_labels][:,[self.color_ind, self.layer_ind, self.size_ind, self.pos1_ind, self.pos1_ind+1, self.pos1_ind+2]]
+        self.lines_labels = np.nonzero(all_objects[:,0] == command_coding['l'])[0]
         self.painter_methods[self.lines_labels,0] = 2
-        self.sticks_labels = np.nonzero(self.objects[:,0] == command_coding['s'])[0]
+        self.lines = all_objects[self.lines_labels][:,[self.color_ind, self.layer_ind, self.size_ind, self.pos1_ind, self.pos1_ind+1, self.pos1_ind+2,self.pos2_ind, self.pos2_ind+1, self.pos2_ind+2]]
+        self.sticks_labels = np.nonzero(all_objects[:,0] == command_coding['s'])[0]
         self.painter_methods[self.sticks_labels,0] = 2
+        self.sticks = all_objects[self.sticks_labels][:,[self.color_ind, self.layer_ind, self.size_ind, self.pos1_ind, self.pos1_ind+1, self.pos1_ind+2,self.pos2_ind, self.pos2_ind+1, self.pos2_ind+2]]
 
         self.fidelity = fidelity_scale[0]
+
+        self.pos1_ind = 3
+        self.pos2_ind = 6
+
+        self.size_ind = 2
+        self.color_ind = 0
+        self.layer_ind = 1
 
         self.painter_methods[self.circles_labels, 1] = self.getCirclesPens()
         self.painter_methods[self.circles_labels, 2] = self.getCirclesBrushes()
@@ -88,8 +94,9 @@ class homerFrame:
         self.painter_methods[self.lines_labels, 2] = self.getLinesBrushes()
         self.painter_methods[self.sticks_labels, 1] = self.getSticksPens()
         self.painter_methods[self.sticks_labels, 2] = self.getSticksBrushes()
-        
+        print self.painter_methods[:10]
         self.width_scale = 1
+
 
     def getCirclesPens(self):
         pcolor = Qt.black
@@ -102,19 +109,16 @@ class homerFrame:
 
 
     def getCirclesBrushes(self):
-        c = self.objects[self.circles_labels]
-        return self.getBrush(c)
+        return self.getBrush(self.circles)
         
     def getLinesPens(self):
-        l = self.objects[self.lines_labels]
-        return self.getPen(l)
+        return self.getPen(self.lines)
 
     def getLinesBrushes(self):
         return QBrush(Qt.SolidPattern)
 
     def getSticksPens(self):
-        s = self.objects[self.sticks_labels]
-        return self.getPen(s)
+        return self.getPen(self.sticks)
 
     def getSticksBrushes(self):
         return QBrush(Qt.SolidPattern)
@@ -124,29 +128,29 @@ class homerFrame:
         transformed_positions_1 = np.dot(self.bare_positions[:,0:3],transform)
         transformed_positions_2 = np.dot(self.bare_positions[:,3:6],transform)
 
-        self.objects[:,self.pos1_ind:self.pos1_ind+6] = np.hstack((transformed_positions_1, transformed_positions_2))
-        self.objects[:,self.size_ind] = self.bare_sizes*np.linalg.det(transform)**(1./3.)
+        all_objects[:,self.pos1_ind:self.pos1_ind+6] = np.hstack((transformed_positions_1, transformed_positions_2))
+        all_objects[:,self.size_ind] = self.bare_sizes*np.linalg.det(transform)**(1./3.)
 
 
 
     def getLineF(self,v):
-        return np.array([QLineF(a[pos1_ind], a[pos1_ind+2], a[pos2_ind], a[pos2_ind+2]) for a in v])
+        return np.array([QLineF(a[self.pos1_ind], a[self.pos1_ind+2], a[self.pos2_ind], a[self.pos2_ind+2]) for a in v])
 
 
     def getRectF(self,v):
-        rad = v[:,size_ind]
-        v[:,pos1_ind] = v[:,pos1_ind]-rad
-        v[:,pos1_ind+2] = v[:,pos1_ind+2]-rad
-        return np.array([ QRectF(a[pos1_ind], a[pos1_ind+2], 2*a[size_ind], 2*a[size_ind]) for a in v ])
+        rad = v[:,self.size_ind]
+        v[:,self.pos1_ind] = v[:,self.pos1_ind]-rad
+        v[:,self.pos1_ind+2] = v[:,self.pos1_ind+2]-rad
+        return np.array([ QRectF(a[self.pos1_ind], a[self.pos1_ind+2], 2*a[self.size_ind], 2*a[self.size_ind]) for a in v ])
 
     def getBrush(self,v):
         fid = fidelity_scale[self.fidelity]
-        c = self.colordef[v[:,color_ind].astype(np.int)]
+        c = self.colordef[v[:,self.color_ind].astype(np.int)]
         return np.array([ QBrush(col,fid) for col in c ])
     
     def getPen(self,v):
-        c = self.colordef[v[:,color_ind].astype(np.int)]
-        w = v[:,size_ind]
+        c = self.colordef[v[:,self.color_ind].astype(np.int)]
+        w = v[:,self.size_ind]
         pens = np.array([ QPen(col) for col in c ])
         for i in range(len(pens)):
             p = pens[i]
