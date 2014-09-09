37cce92e195fe322fef4c24bee8c29a415094cc0
Romain Mari <romain.mari@gmail.com>
2014-09-09 10:30:37 -0400

    Improved performance of homerFrame class

--- src/homerFrame.py
+++ src/homerFrame.py
@@ -24,7 +24,7 @@ import sys
 #import numba
 
 command_coding = { 'y':0, '@':1, 'r':3, 'c':4, 'l':5, 's':6 }
-fidelity_scale = [ Qt.SolidPattern, Qt.Dense2Pattern, Qt.Dense6Pattern, Qt.NoBrush ]
+fidelity_scale = [ Qt.SolidPattern, Qt.Dense3Pattern, Qt.Dense6Pattern, Qt.NoBrush ]
 
 
 size_ind = 7
@@ -44,7 +44,7 @@ color_ind = 8
 
 class homerFrame(object):
 
-    __slots__ = [ 'pos2_ind', 'width_scale', 'size_ind', 'fidelity', 'sticks', 'layers', 'lines_labels', 'sticks_labels', 'pos1_ind', 'circles', 'circles_labels', 'layer_ind', 'obj_nb', 'painter_methods', 'painter', 'layer_list', 'color_ind', 'lines', 'colordef', 'ordering', 'transform'] # saves some memory usage by avoiding dict of attributes
+    __slots__ = [ 'pos2_ind', 'size_ind', 'fidelity', 'sticks', 'layers', 'lines_labels', 'sticks_labels', 'pos1_ind', 'circles', 'circles_labels', 'layer_ind', 'obj_nb', 'painter_methods', 'painter', 'layer_list', 'color_ind', 'lines', 'colordef', 'ordering', 'transform', 'scale'] # saves some memory usage by avoiding dict of attributes
 
     def __init__(self, obj):
         self.colordef = np.array([Qt.black, Qt.gray, Qt.white, Qt.green, Qt.yellow, Qt.red, Qt.blue])
@@ -62,7 +62,7 @@ class homerFrame(object):
         bare_sizes = np.zeros(obj_nb)
         for i in range(len(size_pos)-1):
             bare_sizes[size_pos[i]:size_pos[i+1]] = obj[size_pos[i],1]
-#        print bare_sizes.shape, obj.shape, size_pos.shape, size_pos[-1]
+
         bare_sizes[size_pos[-1]:] = obj[size_pos[-1],1]
 
         color_pos = np.nonzero(obj[:,0] == command_coding['@'])[0]
@@ -89,7 +89,7 @@ class homerFrame(object):
 
         self.layers = all_objects[:,self.layer_ind]
 
-        self.painter_methods = np.empty((self.obj_nb,7), dtype=np.object)
+        self.painter_methods = np.empty((self.obj_nb,8), dtype=np.object)
 
         self.circles_labels = np.nonzero(all_objects[:,0] == command_coding['c'])[0]
         self.painter_methods[self.circles_labels,0] = 1
@@ -108,15 +108,8 @@ class homerFrame(object):
 
         self.size_ind = 1
         self.color_ind = 0
-#        self.layer_ind = 1
 
-#        self.painter_methods[self.circles_labels, 1] = self.getCirclesPens()
-#        self.painter_methods[self.circles_labels, 2] = self.getCirclesBrushes()
-#        self.painter_methods[self.lines_labels, 1] = self.getLinesPens()
-#        self.painter_methods[self.lines_labels, 2] = self.getLinesBrushes()
-#        self.painter_methods[self.sticks_labels, 1] = self.getSticksPens()
-#        self.painter_methods[self.sticks_labels, 2] = self.getSticksBrushes()
-        self.width_scale = 1
+        self.scale = 1
 
 
     def getCirclesPens(self):
@@ -156,44 +149,47 @@ class homerFrame(object):
         transformed_circles_sizes = self.circles[:,self.size_ind]*scale
 
         # 2 generate associated qt geometric shapes
-#        objectAttrs = self.getRectF(transformed_circles_positions, transformed_circles_sizes)
-#        self.painter_methods[self.circles_labels,3] = objectAttrs
         pr = np.column_stack((transformed_circles_positions,transformed_circles_sizes))
         pr[:,0] = pr[:,0] - pr[:,3]
         pr[:,2] = -pr[:,2] - pr[:,3]
         pr[:,3] = 2*pr[:,3]
-#        transformed_circles_positions[:,0] = np.reshape(np.ravel(transformed_circles_positions[:,0]) - transformed_circles_sizes, (transformed_circles_sizes.size,1))
-#        transformed_circles_positions[:,2] = -transformed_circles_positions[:,2] - transformed_circles_sizes
-#        transformed_circles_sizes = 2*transformed_circles_sizes
-        self.painter_methods[self.circles_labels,3] = np.ravel(pr[:,0])
-        self.painter_methods[self.circles_labels,4] = np.ravel(pr[:,2])
-        self.painter_methods[self.circles_labels,5] = np.ravel(pr[:,3])
-        self.painter_methods[self.circles_labels,6] = np.ravel(pr[:,3])
+        p1 = 4
+        p2 = 5
+        p3 = 6
+        p4 = 7
+        self.painter_methods[self.circles_labels,p1] = np.ravel(pr[:,0])
+        self.painter_methods[self.circles_labels,p2] = np.ravel(pr[:,2])
+        self.painter_methods[self.circles_labels,p3] = np.ravel(pr[:,3])
+        self.painter_methods[self.circles_labels,p4] = np.ravel(pr[:,3])
         
 
-#        objectAttrs = self.getLineF(transformed_lines_positions)
-#        self.painter_methods[self.lines_labels,3] = objectAttrs
-        self.painter_methods[self.lines_labels,3] = np.ravel(transformed_lines_positions[:,0])
-        self.painter_methods[self.lines_labels,4] = -np.ravel(transformed_lines_positions[:,2])
-        self.painter_methods[self.lines_labels,5] = np.ravel(transformed_lines_positions[:,3])
-        self.painter_methods[self.lines_labels,6] = -np.ravel(transformed_lines_positions[:,5])
+        self.painter_methods[self.lines_labels,p1] = np.ravel(transformed_lines_positions[:,0])
+        self.painter_methods[self.lines_labels,p2] = -np.ravel(transformed_lines_positions[:,2])
+        self.painter_methods[self.lines_labels,p3] = np.ravel(transformed_lines_positions[:,3])
+        self.painter_methods[self.lines_labels,p4] = -np.ravel(transformed_lines_positions[:,5])
 
 
-#        objectAttrs = self.getLineF(transformed_sticks_positions)
-#        self.painter_methods[self.sticks_labels,3] = objectAttrs
-        self.painter_methods[self.sticks_labels,3] = np.ravel(transformed_sticks_positions[:,0])
-        self.painter_methods[self.sticks_labels,4] = -np.ravel(transformed_sticks_positions[:,2])
-        self.painter_methods[self.sticks_labels,5] = np.ravel(transformed_sticks_positions[:,3])
-        self.painter_methods[self.sticks_labels,6] = -np.ravel(transformed_sticks_positions[:,5])
+        self.painter_methods[self.sticks_labels,p1] = np.ravel(transformed_sticks_positions[:,0])
+        self.painter_methods[self.sticks_labels,p2] = -np.ravel(transformed_sticks_positions[:,2])
+        self.painter_methods[self.sticks_labels,p3] = np.ravel(transformed_sticks_positions[:,3])
+        self.painter_methods[self.sticks_labels,p4] = -np.ravel(transformed_sticks_positions[:,5])
 
-        self.painter_methods[self.circles_labels, 1] = self.getCirclesPens()
-        self.painter_methods[self.circles_labels, 2] = self.getBrush(self.circles)
-        self.painter_methods[self.lines_labels, 1] = self.getPen(self.lines)
-        self.painter_methods[self.lines_labels, 2] = QBrush(Qt.SolidPattern)
-        self.painter_methods[self.sticks_labels, 1] = self.getPen(self.sticks)
-        self.painter_methods[self.sticks_labels, 2] = QBrush(Qt.SolidPattern)
+        pcolor = 1
+        pthickness = 2
+        bcolor = 3
+        self.painter_methods[self.circles_labels, pcolor] = Qt.black
+        self.painter_methods[self.circles_labels, pthickness] = 1
+        self.painter_methods[self.circles_labels, bcolor] = self.colordef[self.circles[:,self.color_ind].astype(np.int)]
 
+        self.painter_methods[self.lines_labels, pcolor] = self.colordef[self.lines[:,self.color_ind].astype(np.int)]
+        self.painter_methods[self.lines_labels, pthickness] = 1
+        self.painter_methods[self.lines_labels, bcolor] = Qt.black
 
+        spainters = self.painter_methods[self.sticks_labels]
+        self.painter_methods[self.sticks_labels, pcolor] = self.colordef[self.sticks[:,self.color_ind].astype(np.int)]
+        self.painter_methods[self.sticks_labels, pthickness] = self.scale*self.sticks[:,self.size_ind]
+        self.painter_methods[self.sticks_labels, bcolor] = Qt.black
+                
         # 3 order according to z coord
         z_coords = np.zeros(self.obj_nb)
         z_coords[self.circles_labels] = -transformed_circles_positions[:,1] 
@@ -211,12 +207,6 @@ class homerFrame(object):
         pcalls[ pcalls[:,0] == 1,0] = self.painter.drawEllipse 
         pcalls[ pcalls[:,0] == 2,0] = self.painter.drawLine
         
-        self.painter_methods[self.circles_labels, 1] = None
-        self.painter_methods[self.circles_labels, 2] = None
-        self.painter_methods[self.lines_labels, 1] = None
-        self.painter_methods[self.lines_labels, 2] = None
-        self.painter_methods[self.sticks_labels, 1] = None
-        self.painter_methods[self.sticks_labels, 2] = None
         return pcalls
 
     def display(self, painter, transform, layer_list, fidelity):
@@ -224,18 +214,21 @@ class homerFrame(object):
         self.painter = painter
         self.layer_list = layer_list
         self.transform = transform
+        self.scale = np.linalg.det(transform)**(1./3.)
+    
+        pen = QPen()
+        brush = QBrush()
+        brush.setStyle(self.fidelity)
 
-        self.width_scale = (np.linalg.det(self.transform)**(1./3.))
-
-        for [paintMethod, pen, brush, paintArgs0, paintArgs1, paintArgs2, paintArgs3] in self.generatePainters():
-            pen.setWidthF(self.width_scale*pen.widthF())
+        for [paintMethod, pcolor, pthickness, bcolor, paintArgs0, paintArgs1, paintArgs2, paintArgs3] in self.generatePainters():
+            pen.setColor(pcolor)
+            pen.setWidthF(pthickness)
             painter.setPen(pen)
 
-            brush.setStyle(self.fidelity)
+            brush.setColor(bcolor)
             painter.setBrush(brush)
+
             paintMethod(paintArgs0, paintArgs1, paintArgs2, paintArgs3)
-            pen.setWidthF(pen.widthF()/self.width_scale)
-#        paintFrameEvent(self,generatePainters(self))
     
         self.ordering = np.array([])
 
