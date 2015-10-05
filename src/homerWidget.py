dc3c21157072a4d28b2919812d768e0e41ec998a
Romain Mari <rmari@ccny.cuny.edu>
2015-10-05 17:37:07 +0100

    adding rotation via arrow keys

--- src/homerWidget.py
+++ src/homerWidget.py
@@ -310,6 +310,34 @@ class homerWidget(QGLWidget):
             except ValueError:
                 self.target_layer = "all"
             catched = True
+        elif e == Qt.Key_Up and m != Qt.SHIFT:
+            angleX = 0.1
+            sinAngleX = np.sin(angleX)
+            cosAngleX = np.cos(angleX)
+            generator = np.mat([[1, 0, 0], [0, cosAngleX, -sinAngleX], [0, sinAngleX, cosAngleX]])
+            self.transform = generator*self.transform
+            catched = True
+        elif e == Qt.Key_Down and m != Qt.SHIFT:
+            angleX = -0.1
+            sinAngleX = np.sin(angleX)
+            cosAngleX = np.cos(angleX)
+            generator = np.mat([[1, 0, 0], [0, cosAngleX, -sinAngleX], [0, sinAngleX, cosAngleX]])
+            self.transform = generator*self.transform
+            catched = True
+        elif e == Qt.Key_Left and m != Qt.SHIFT:
+            angleY = -0.1
+            sinAngleY = np.sin(angleY)
+            cosAngleY = np.cos(angleY)
+            generator = np.mat([[cosAngleY, -sinAngleY, 0], [sinAngleY, cosAngleY, 0], [0, 0, 1]])
+            self.transform = generator*self.transform
+            catched = True
+        elif e == Qt.Key_Right and m != Qt.SHIFT:
+            angleY = 0.1
+            sinAngleY = np.sin(angleY)
+            cosAngleY = np.cos(angleY)
+            generator = np.mat([[cosAngleY, -sinAngleY, 0], [sinAngleY, cosAngleY, 0], [0, 0, 1]])
+            self.transform = generator*self.transform
+            catched = True
 
         t = event.text()
         try:
